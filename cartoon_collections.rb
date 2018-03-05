a98fff2939ba6b5a5740825e542ce206c7915af1
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-03-05 17:24:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find { |cheese| cheese == }
+  array.find { |cheese| cheese == "cheddar" || "gouda" || "camembert" }
 end
